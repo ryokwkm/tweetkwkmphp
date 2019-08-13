55be08c852ccac53bdb6ec50cecd2cc2654a0ce6
Ryo Kawakami <ryo.kawakami@miidas.jp>
2019-08-13 16:40:38 +0900

    table名を動的に

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Api extends CI_Controller {
+class Api extends MY_Controller {
 
 	public $beforeDay;
 	public $country;
@@ -17,12 +17,17 @@ class Api extends CI_Controller {
 
 		$this->beforeDay = $beforeDay;
 		$this->country = $country;
+		$this->appName = $this->input->get("v");
 
 
 		$topArticles = $this->getTopArticles();
 		if (empty($topArticles)) {
 			$this->beforeDay += 1;
 			$topArticles = $this->getTopArticles();
+			if (empty($topArticles)) {
+				$this->beforeDay += 1;
+				$topArticles = $this->getTopArticles();
+			}
 		}
 
 		//html特殊文字を変換
@@ -37,71 +42,7 @@ class Api extends CI_Controller {
 		echo json_encode($topArticles);
 	}
 
-	//情報取得
-	private function getTopArticles() {
-		$tableName = $this->getTable();
-		$targetStart = date('Y-m-d', mktime(0, 0, 0, date('n'), date('j') - $this->beforeDay, date('Y')));
-		$targetEnd = date('Y-m-d', mktime(0, 0, 0, date('n'), date('j') - $this->beforeDay + 1, date('Y')));
 
-		$this->load->database();
-		$query = $this->db->query(
-			"SELECT * FROM {$tableName}
-			WHERE created >= ? AND created < ?
-			AND country = ?
-			AND parent_id = 0",
-			array($targetStart, $targetEnd, $this->country));
-		return $query->result_array();
-	}
 
-	private function getChildArticles($topArticles) {
-		$tableName = $this->getTable();
-		$targetStart = date('Y-m-d', mktime(0, 0, 0, date('n'), date('j') - $this->beforeDay, date('Y')));
-		$targetEnd = date('Y-m-d', mktime(0, 0, 0, date('n'), date('j') - $this->beforeDay + 1, date('Y')));
-
-		foreach($topArticles as $key => $topArticle ) {
-			$reactions = $this->db->query(
-				"SELECT * FROM {$tableName}
-				WHERE created >= ? AND created < ?
-				AND country = ?
-				AND parent_id = ?",
-				array($targetStart, $targetEnd, $this->country, $topArticle["id"]))->result_array();
-
-			//いいね順にソート。Html特殊文字変換
-			$reactions = $this->htmlDecode($reactions);
-			$reactions = $this->sortById($reactions, "favorite");
-			$topArticles[$key]["reactions"] = $reactions;
-		}
-		return $topArticles;
-	}
-
-	//ソート
-	private function sortById($articles, $keyName="id") {
-		if(empty($articles)) return $articles;
-		foreach ($articles as $key => $value) {
-		    $sort[$key] = $value[$keyName];
-		}
-
-		array_multisort($sort, SORT_DESC, $articles);
-		return $articles;
-	}
-
-	//html特殊文字をもとに戻す
-	private function htmlDecode($articles) {
-		foreach ($articles as $key => $article) {
-			$articles[$key]["head"] = str_replace(array('&nbsp;', '&emsp;', '&ensp;'), '', $articles[$key]["head"]);
-			$articles[$key]["body"] = str_replace(array('&nbsp;', '&emsp;', '&ensp;'), '', $articles[$key]["body"]);
-			$articles[$key]["head"] = htmlspecialchars_decode($articles[$key]["head"], ENT_QUOTES);
-			$articles[$key]["body"] = htmlspecialchars_decode($articles[$key]["body"], ENT_QUOTES);
-		}
-		return $articles;
-	}
-
-	private function getTable() {
-		if(IsProduction()) {
-			return "t_sportskwkm_tweet_logs";
-		} else {
-			return "t_kwkmlight_tweet_logs";
-		}
-	}
 
 }
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,84 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller {
+	public $beforeDay;
+	public $country;
+	public $appName;	//twitter_usersテーブルのID
+
+
+	//情報取得
+	protected function getTopArticles() {
+		$tableName = $this->getTable();
+		$targetStart = date('Y-m-d', mktime(0, 0, 0, date('n'), date('j') - $this->beforeDay, date('Y')));
+		$targetEnd = date('Y-m-d', mktime(0, 0, 0, date('n'), date('j') - $this->beforeDay + 1, date('Y')));
+
+		$this->load->database();
+		$query = $this->db->query(
+			"SELECT * FROM {$tableName}
+			WHERE created >= ? AND created < ?
+			AND country = ?
+			AND parent_id = 0",
+			array($targetStart, $targetEnd, $this->country));
+		$result = $query->result_array();
+
+		return $result;
+	}
+
+	protected function getChildArticles($topArticles) {
+		$tableName = $this->getTable();
+		$targetStart = date('Y-m-d', mktime(0, 0, 0, date('n'), date('j') - $this->beforeDay, date('Y')));
+		$targetEnd = date('Y-m-d', mktime(0, 0, 0, date('n'), date('j') - $this->beforeDay + 1, date('Y')));
+
+		foreach($topArticles as $key => $topArticle ) {
+			$reactions = $this->db->query(
+				"SELECT * FROM {$tableName}
+				WHERE created >= ? AND created < ?
+				AND country = ?
+				AND parent_id = ?",
+				array($targetStart, $targetEnd, $this->country, $topArticle["id"]))->result_array();
+
+			//いいね順にソート。Html特殊文字変換
+			$reactions = $this->htmlDecode($reactions);
+			$reactions = $this->sortById($reactions, "favorite");
+			$topArticles[$key]["reactions"] = $reactions;
+		}
+		return $topArticles;
+	}
+
+	//ソート
+	protected function sortById($articles, $keyName="id") {
+		if(empty($articles)) return $articles;
+		foreach ($articles as $key => $value) {
+		    $sort[$key] = $value[$keyName];
+		}
+
+		array_multisort($sort, SORT_DESC, $articles);
+		return $articles;
+	}
+
+	//html特殊文字をもとに戻す
+	protected function htmlDecode($articles) {
+		foreach ($articles as $key => $article) {
+			$articles[$key]["head"] = str_replace(array('&nbsp;', '&emsp;', '&ensp;'), '', $articles[$key]["head"]);
+			$articles[$key]["body"] = str_replace(array('&nbsp;', '&emsp;', '&ensp;'), '', $articles[$key]["body"]);
+			$articles[$key]["head"] = htmlspecialchars_decode($articles[$key]["head"], ENT_QUOTES);
+			$articles[$key]["body"] = htmlspecialchars_decode($articles[$key]["body"], ENT_QUOTES);
+		}
+		return $articles;
+	}
+
+
+	protected function getTable() {
+		// if(true){	//if(IsProduction()) {
+		// 	return "t_sportskwkm_tweet_logs";
+		// }
+		$this->load->database();
+		$query = $this->db->query(
+			"SELECT * FROM twitter_users
+			WHERE id = ?",$this->appName);
+		if(!empty($result = $query->result_array())) {
+			return "t_". $result[0]["account_name"]. "_tweet_logs";
+		}
+	}
+}
