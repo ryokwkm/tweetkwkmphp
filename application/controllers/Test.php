f629fc5e0ee65fa30c67de2e6a267bce373294ee
Ryo Kawakami <ryo.kawakami@miidas.jp>
2020-03-09 22:10:31 +0900

    test

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -17,7 +17,15 @@ class Test extends CI_Controller {
 		$request_token = [];  // [] は array() の短縮記法。詳しくは以下の「追々記」参照
 		$request_token['oauth_token'] = $_SESSION['oauth_token'];
 		$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+		//OAuth トークンも用いて TwitterOAuth をインスタンス化
+		$app_name = $_SESSION['account_name'];
+		$twitterApps = $this->getApp();
+		$twitterApp = $twitterApps[0];
 
 	}
-
+	//情報取得
+	protected function getApp() {
+		$query = $this->db->query("SELECT * from twitter_apps where id = 14");
+		return $query->result_array();
+	}
 }
