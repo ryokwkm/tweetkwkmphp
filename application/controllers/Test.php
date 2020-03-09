5ca4da215c9947e7370cabfaa3f3259943240c48
Ryo Kawakami <ryo.kawakami@miidas.jp>
2020-03-09 21:47:10 +0900

    test

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -10,7 +10,7 @@ class Test extends CI_Controller {
 	 */
 	public function index()
 	{
-		echo APPPATH;
+		echo "ok";
 		$this->load->library('session');
 		vr($_SESSION);
 		$this->load->view('auth/index.html');
