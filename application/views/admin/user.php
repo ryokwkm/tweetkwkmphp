bec68ca93747deb394bde79dc0b0b4b6d9d7b2a8
Ryo Kawakami <ryo.kawakami@miidas.jp>
2020-03-10 19:32:16 +0900

    scss

--- application/views/admin/user.php
+++ application/views/admin/user.php
@@ -1,6 +1,23 @@
 
 <div class="container-fluid">
 	<div class="row">
+		<div class="col-md-4">
+			<div class="card card-profile">
+				<div class="card-avatar">
+					<a href="#pablo">
+						<img class="img" src="../assets/img/faces/marc.jpg" />
+					</a>
+				</div>
+				<div class="card-body">
+					<h6 class="card-category text-gray">CEO / Co-Founder</h6>
+					<h4 class="card-title">Alec Thompson</h4>
+					<p class="card-description">
+						Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
+					</p>
+					<a href="#pablo" class="btn btn-primary btn-round">Follow</a>
+				</div>
+			</div>
+		</div>
 		<div class="col-md-8">
 			<div class="card">
 				<div class="card-header card-header-primary">
@@ -88,22 +105,6 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-md-4">
-			<div class="card card-profile">
-				<div class="card-avatar">
-					<a href="#pablo">
-						<img class="img" src="../assets/img/faces/marc.jpg" />
-					</a>
-				</div>
-				<div class="card-body">
-					<h6 class="card-category text-gray">CEO / Co-Founder</h6>
-					<h4 class="card-title">Alec Thompson</h4>
-					<p class="card-description">
-						Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
-					</p>
-					<a href="#pablo" class="btn btn-primary btn-round">Follow</a>
-				</div>
-			</div>
-		</div>
+
 	</div>
 </div>
