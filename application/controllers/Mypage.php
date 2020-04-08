ebb190b88976473c16f4a84a314294c7023ca5dd
Ryo Kawakami <ryo.kawakami@miidas.jp>
2020-04-08 19:04:47 +0900

    サーバー上の不具合修正

--- application/controllers/Mypage.php
+++ application/controllers/Mypage.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MyPage extends MY_Controller {
+class Mypage extends MY_Controller {
 	public function __construct()
 	{
 		parent::__construct();
