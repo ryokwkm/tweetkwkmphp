6535836dd9ecba82efae5fd00966e989179e6d55
Ryo Kawakami <ryo.kawakami@miidas.jp>
2020-11-18 12:50:39 +0900

    fix

--- application/models/Laction_model.php
+++ application/models/Laction_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class LAction_model extends CI_Model {
+class Laction_model extends CI_Model {
 	public function __construct()
 	{
 		parent::__construct();
